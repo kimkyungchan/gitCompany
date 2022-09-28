bfb9583dc8e8ce7c7b95fb9d4f75c201fc5a0578
김경찬 <rlarudcks0339@naver.com>
2022-09-28 09:36:56 +0900

    ajax, jquery

---
+++ WebContent/day0927/jquery.rollover.js
@@ -0,0 +1,24 @@
+$.fn.rollover = function() {
+
+	$(this).mouseover(
+		function() {
+			$(this).attr(
+				"src",
+				$(this).attr("src").replace(/^(.+)(\.[a-z]+)$/,
+					"$1_on$2"));
+		}).mouseout(
+			function() {
+				$(this).attr(
+					"src",
+					$(this).attr("src").replace(
+						/^(.+)_on(\.[a-z]+)$/, "$1$2"));
+			}).each(
+				function() {
+					$("<img>").attr(
+						"src",
+						$(this).attr("src").replace(/^(.+)(\.[a-z]+)$/,
+							"$1_on$2"));
+				});
+
+	return this;
+}
